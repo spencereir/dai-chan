035e84597ce1f84facdde972ba3031066b5ca81a
Spencer Whitehead <spencer.whitehead@rogers.com>
2017-12-18 17:57:20 -0600

    fixed the thing

--- commands/general.py
+++ commands/general.py
@@ -6,9 +6,10 @@ def say(message):
 logmode = ""
 
 async def log(message, client):
-    await client.send_message(message.channel, "[{}|{0: <10}|{:%Y-%m-%d %H:%M:%S}] {}".format(logmode, message.author.name, datetime.datetime.now(), message.content))
+    await client.send_message(message.channel, "[{}|{:<10}|{:%Y-%m-%d %H:%M:%S}] {}".format(logmode, message.author.name, datetime.datetime.now(), message.content))
     await client.delete_message(message)
 
 async def set_logmode(message, client):
     global logmode
     logmode = message
+    await client.send_message(message.channel, "Changed log mode to {}".format(logmode))
